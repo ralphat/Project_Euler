c2c729de73c18e8457dd4044fbf3892c34da0fdb
ralphat <ralphat@users.noreply.github.com>
2016-05-20 00:39:34 +0530

    Update 003_Largest Prime Factor.py

--- 003_Largest Prime Factor.py
+++ 003_Largest Prime Factor.py
@@ -6,9 +6,11 @@ for i in range(t):
         n = n/2
         h = 2
     i = 3
-    while i<m:
+    while i<=int(m**0.5):
         while n%i==0:
             n = n/i
             h = i
         i = i+2
+    if n>h:
+        h = n
     print h
